633b43e2bc47510a5bad51cec12f6a8a36cb8cd9
Louis Weigel <account@louisweigel.com>
2023-04-29 17:54:03 +0200

    Initial Commit

---
+++ I am bored/I_am_boredApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_boredApp.swift
+//  I am bored
+//
+//  Created by Louis Weigel on 29.04.23.
+//
+
+import SwiftUI
+
+@main
+struct I_am_boredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
